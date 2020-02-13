99a7ebca1473dbc389baa5a0a1f5b32c5b9f84ae
LinusBF <contact@linusbf.com>
2020-02-13 16:27:44 +0100

    Test for dev CI

--- index.js
+++ index.js
@@ -2,38 +2,23 @@
 
 const express = require('express');
 const app = express();
-
-const database = require('./src/database');
+//const attachJobApplicationEndpoints = require('./src/jobApplication/moduleRouter');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-  console.log('Hello world received a request.');
+const router = express.Router();
 
-  res.status(200).send({data: 'This is backend talking via GET'});
+router.get('/', (req, res) => {
+  console.log('Received a request at root?');
+  res.status(200).send({
+    data: 'Please make a request to a valid endpoint',
+    envChecker: process.env.JOB_APPLICATION_KIND,
+  });
 });
 
-app.post('/', (req, res) => {
-  console.log('Hello world received a POST request.');
-  console.info(`Received post data: ${req.body}`);
-
-  res.status(200).send({data: 'This is backend talking via POST'});
-});
-
-app.post('/addApplicant', (req, res) => {
-  console.info(`Received post data: ${req.body}`);
-
-  const applicant = req.body;
-  database
-    .putApplicantInDB(applicant)
-    .then(response => {
-      res.status(200).send({data: response});
-    })
-    .catch(err => {
-      res.status(500).send({data: err});
-    });
-});
+//attachJobApplicationEndpoints(router);
+app.use('/', router);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
