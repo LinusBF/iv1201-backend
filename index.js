e190299df255c0ad59a54cb116012b31b41b7b31
LinusBF <contact@linusbf.com>
2020-02-13 17:34:41 +0100

    Fuck this shit

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ router.get('/', (req, res) => {
   console.log('Received a request at root?');
   res.status(200).send({
     data: 'Please make a request to a valid endpoint',
-    envChecker:
-      process.env.NODE_ENV === 'development'
-        ? process.env.JOB_APPLICATION_KIND
-        : process.env.JOB_APPLICATION_KIND_DEV,
+    envChecker: `${process.env.NODE_ENV}`,
   });
 });
 
