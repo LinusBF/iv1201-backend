a99d81527bc8042e4503e410814c060a2668a2d9
LinusBF <contact@linusbf.com>
2020-02-13 20:06:34 +0100

    Added user module

---
+++ src/user/moduleRouter.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const {getApplicationByUser} = require('../jobApplication/jobApplicationController');
+const {validateUserId} = require('../modelValidator');
+
+/**
+ *
+ * @param router express.Router
+ */
+module.exports = router => {
+  router.get('/user/:userId/application', (req, res) => {
+    console.info(`Received request to get application by user`);
+    if (validateUserId(req.params.userId)) {
+      getApplicationByUser(req.params.userId)
+        .then(application => {
+          if (application === false) {
+            res.status(404).send('Could not find job application from that user!');
+          } else {
+            res.status(200).send({application});
+          }
+        })
+        .catch(err => {
+          console.error(err);
+          res.status(500).send('Something failed! Check the logs!');
+        });
+    }
+  });
+};
