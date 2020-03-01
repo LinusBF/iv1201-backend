d3fe4b6ef9266bd88c656888e233a639650a87bb
Erik Lenas <erik.lenas@gmail.com>
2020-03-01 10:02:37 +0100

    trying to fix user status, maybe get request to backend aint working

--- src/user/moduleRouter.js
+++ src/user/moduleRouter.js
@@ -30,7 +30,7 @@ module.exports = router => {
     }
   });
 
-  router.get('/user/:userId/user-status', (req, res) => {
+  router.post('/user/:userId/user-status', (req, res) => {
     console.info(`Received request to get status of user`);
     if (validateUserId(req.params.userId)) {
       getUserStatus(req.params.userId)
